14a9af6b6cbd8f626ecfc8295e9c041b7ad899da
RobertoPavonLariosLabtend <roberto.pavon@labtend.com>
2025-01-20 08:46:09 +0100

    custom safe area created

--- aerox_stage_1/lib/features/feature_home/ui/home_page_admin.dart
+++ aerox_stage_1/lib/features/feature_home/ui/home_page_admin.dart
@@ -1,3 +1,4 @@
+import 'package:aerox_stage_1/features/feature_home/ui/top_notch_padding.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,55 +12,59 @@ class HomePageAdmin extends StatelessWidget {
 
     final userBloc = BlocProvider.of<UserBloc>( context );
 
-    return Scaffold(
-      backgroundColor: backgroundColor,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.only( bottom: 40 ),
-          child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  AppButton( 
-                    text: 'TU PALA AEROX', 
-                    onPressed: () {
-                      // Navigator.push(
-                      // context,
-                      // MaterialPageRoute(builder: (context) => const CatalogScreen()),
-                      // );
-                    },
-                    ),
-                    const SizedBox(height: 30),
-                    AppButton( text: 'TU JUEGO',
-                      onPressed:() {
-          
-                      }
-                    ),
-          
-                    const SizedBox(height: 30),
-          
-                    AppButton(
-                      text: 'JUGAR',
-                      backgroundColor: appYellowColor,
-                      showborder: false,
-                      fontColor: Colors.black,
-                      onPressed: (){
+    return TopNotchPadding(
+      context: context,
+      child: Scaffold(
+        appBar: HomePageAppbar(),
+        backgroundColor: backgroundColor,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.only( bottom: 40 ),
+            child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    AppButton( 
+                      text: 'TU PALA AEROX', 
+                      onPressed: () {
+                        // Navigator.push(
+                        // context,
+                        // MaterialPageRoute(builder: (context) => const CatalogScreen()),
+                        // );
                       },
-                    ),
-          
-                    const SizedBox(height: 30),
-          
-                    // AppButton(
-                    //   text: "SIGN OUT",
-                    //   onPressed: (){
-                    //     userBloc.add( OnGoogleSignOutUser() );
-                    //     userBloc.add( OnEmailSignOutUser() );
-                    //   } 
-                    // ),
-                  ],
-              ),
+                      ),
+                      const SizedBox(height: 30),
+                      AppButton( text: 'TU JUEGO',
+                        onPressed:() {
+            
+                        }
+                      ),
+            
+                      const SizedBox(height: 30),
+            
+                      AppButton(
+                        text: 'JUGAR',
+                        backgroundColor: appYellowColor,
+                        showborder: false,
+                        fontColor: Colors.black,
+                        onPressed: (){
+                        },
+                      ),
+            
+                      const SizedBox(height: 30),
+            
+                      // AppButton(
+                      //   text: "SIGN OUT",
+                      //   onPressed: (){
+                      //     userBloc.add( OnGoogleSignOutUser() );
+                      //     userBloc.add( OnEmailSignOutUser() );
+                      //   } 
+                      // ),
+                    ],
+                ),
+          ),
+        )
         ),
-      )
-      );
+    );
   }
 }
--- aerox_stage_1/lib/features/feature_home/ui/home_page_barrel.dart
+++ aerox_stage_1/lib/features/feature_home/ui/home_page_barrel.dart
@@ -1,2 +1,3 @@
 export 'package:aerox_stage_1/features/feature_home/ui/home_page_admin.dart';
 export 'package:aerox_stage_1/features/feature_login/ui/login_barrel.dart';
+export 'package:aerox_stage_1/features/feature_home/ui/home_page_appbar.dart';
