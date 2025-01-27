2c5154453fea9774a270c28b8fb7fb98d954f780
RobertoPavonLariosLabtend <roberto.pavon@labtend.com>
2025-01-27 14:07:14 +0100

    sqlite dependencies added to project

--- aerox_stage_1/macos/Flutter/GeneratedPluginRegistrant.swift
+++ aerox_stage_1/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import firebase_core
 import google_sign_in_ios
 import path_provider_foundation
 import sign_in_with_apple
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
@@ -17,4 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
