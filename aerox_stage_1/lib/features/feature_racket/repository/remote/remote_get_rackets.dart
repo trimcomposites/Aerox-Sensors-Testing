745dac2b181b5aa490d758c4b19e77dda161cc15
RobertoPavonLariosLabtend <roberto.pavon@labtend.com>
2025-02-19 10:25:56 +0100

    myexception deleted

--- aerox_stage_1/lib/features/feature_racket/repository/remote/remote_get_rackets.dart
+++ aerox_stage_1/lib/features/feature_racket/repository/remote/remote_get_rackets.dart
@@ -1,10 +1,5 @@
-import 'package:aerox_stage_1/common/utils/either_catch.dart';
-import 'package:aerox_stage_1/common/utils/error/err/racket_err.dart';
-import 'package:aerox_stage_1/common/utils/exceptions/exception.dart';
-import 'package:aerox_stage_1/common/utils/typedef.dart';
 import 'package:aerox_stage_1/domain/models/racket.dart';
 import 'package:aerox_stage_1/domain/models/racket_serializer.dart';
-import 'package:dartz/dartz.dart';
 import 'package:http/http.dart' as http;
 
 class RemoteGetRackets {
