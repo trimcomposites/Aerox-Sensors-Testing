12ad6359da0a4292ec14e3a623a812261e693484
RobertoPavonLariosLabtend <roberto.pavon@labtend.com>
2025-02-20 17:09:57 +0100

    comment section ui created

---
+++ aerox_stage_1/lib/features/feature_comments/ui/comment_section.dart
@@ -0,0 +1,87 @@
+import 'package:aerox_stage_1/features/feature_login/ui/login_barrel.dart';
+import 'package:aerox_stage_1/features/feature_comments/ui/add_comment_button.dart';
+
+class CommentSection extends StatelessWidget {
+  const CommentSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      physics: NeverScrollableScrollPhysics(),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Notas',
+                style: TextStyle(fontSize: 30),
+              ),
+              AddCommentButton(),
+            ],
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: 10,
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                //color: Colors.red,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start, 
+                  children: [
+                    Text('Hoy'),
+                    SizedBox( height: 15, ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center, 
+                      children: [
+                        CircleAvatar(radius: 20),
+                        SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Samuel',
+                              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                            ),
+                            Row(
+                              children: [
+                                Text('02.06.25'),
+                                SizedBox(width: 5),
+                                Icon(Icons.location_on_outlined, size: 16),
+                                SizedBox(width: 5),
+                                Text('Málaga'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10), 
+                    Container(
+                      width: double.infinity, 
+                      child: Text(
+                        'Excepteur ea consectetur et mollit mollit. Nulla eu deserunt laboris nostrud '
+                        'culpa id do pariatur mollit aliquip excepteur proident reprehenderit in. Minim magna i'
+                        'n pariatur dolore mollit sint pariatur. Labore eiusmod labore do exercitation amet nostrud consectetur occaecat e'
+                        'xcepteur commodo reprehenderit. Lorem est consectetur dolore mollit nisi elit est occaecat fugiat. Voluptate sunt des'
+                        'erunt cillum eiusmod veniam adipisicing.',
+                        maxLines: 6,
+                        overflow: TextOverflow.ellipsis,
+                        softWrap: true,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
