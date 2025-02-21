97c8925b7f182ca02a279f00ce45e8a03f5e0920
RobertoPavonLariosLabtend <roberto.pavon@labtend.com>
2025-02-21 10:00:17 +0100

    3d pause

--- aerox_stage_1/lib/features/feature/3d/ui/racket_3d_model.dart
+++ aerox_stage_1/lib/features/feature/3d/ui/racket_3d_model.dart
@@ -4,6 +4,7 @@ import 'package:aerox_stage_1/features/feature/3d/blocs/bloc/3d_bloc.dart';
 import 'package:aerox_stage_1/features/feature_login/ui/login_barrel.dart';
 import 'package:flutter_3d_controller/flutter_3d_controller.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:model_viewer_plus/model_viewer_plus.dart';
 
 class Racket3dModel extends StatelessWidget {
@@ -24,44 +25,54 @@ class Racket3dModel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final model3dBloc = BlocProvider.of<Model3DBloc>( context );
+    final Flutter3DController controller = Flutter3DController();
+    final model3dBloc = BlocProvider.of<Model3DBloc>(context);
     return IgnorePointer(
       ignoring: ignorePointer,
       child: Container(
-          height: 600,
-          width: 400,
-          child: BlocBuilder<Model3DBloc, Model3DState>(
-            builder: (context, state) {
-              return state.uiState.status == UIStatus.error
-              ? CircularProgressIndicator()
-              : Stack(
-                alignment: Alignment.center,
-                children: [
-                  Flutter3DViewer(
-                    activeGestureInterceptor: true,
-                    progressBarColor: Colors.orange,
-                    enableTouch: true,
-                  
-                    onProgress: (double progressValue) {
-                      model3dBloc.add( OnStartLoadingModel3d() );
-                      debugPrint('model loading progress : $progressValue');
-                    },
-                    onLoad: (String modelAddress) {
-                      model3dBloc.add( OnStopLoadingModel3d() );
-                      debugPrint('model loaded : $modelAddress');
-                    },
-                    onError: (String error) {
-                      model3dBloc.add( OnStartErrorModel3d() );
-                      debugPrint('model failed to load : $error');
-                    },
-                    src: 'assets/3d/adidas_padel_2023.glb',
-                  ),
-                  if (state.uiState.status == UIStatus.loading)
-                  CircularProgressIndicator()
-                ],
+        height: 600,
+        width: 400,
+        child: BlocBuilder<Model3DBloc, Model3DState>(
+          builder: (context, state) {
+            if (state.uiState.status == UIStatus.error) {
+              return Image.asset(
+                'assets/Pala-Catalogo.png',
+                fit: BoxFit.fill,
               );
-            },
-          )),
+            }
+            return Stack(
+              alignment: Alignment.center,
+              children: [
+                Flutter3DViewer(
+                  activeGestureInterceptor: ignorePointer,
+                  progressBarColor: Colors.transparent,
+                  enableTouch: true,
+                  onProgress: (double progressValue) {
+                    if (progressValue < 1.0) {
+                      model3dBloc.add(OnStartLoadingModel3d());
+                    }
+                    debugPrint('model loading progress: $progressValue');
+                  },
+                  onLoad: (String modelAddress) {
+                    model3dBloc.add(OnStopLoadingModel3d());
+                    debugPrint('model loaded: $modelAddress');
+                    //controller.setCameraOrbit(5.0, 0, 0);
+                    controller.playAnimation();
+                  },
+                  onError: (String error) {
+                    model3dBloc.add(OnStartErrorModel3d());
+                    debugPrint('model failed to load: $error');
+                  },
+                  src: 'assets/3d/morello_cerchi_-_rims_-_murgese_v.glb',
+                  controller: controller,
+                ),
+                if (state.uiState.status == UIStatus.loading)
+                  CircularProgressIndicator(),
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
