a45c4500775e84734e823dcaf906d3a7705a342d
RobertoPavonLariosLabtend <roberto.pavon@labtend.com>
2025-02-25 10:56:59 +0100

    dateString

--- aerox_stage_1/lib/features/feature_comments/ui/additional_comment_form_info.dart
+++ aerox_stage_1/lib/features/feature_comments/ui/additional_comment_form_info.dart
@@ -1,12 +1,16 @@
 import 'package:aerox_stage_1/features/feature_comments/blocs/bloc/comments_bloc.dart';
 import 'package:aerox_stage_1/features/feature_login/ui/login_barrel.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 class AdditionalCommentFormInfo extends StatelessWidget {
   const AdditionalCommentFormInfo({
     super.key,
   });
-
+  String getDate() {
+    DateTime dateTime = DateTime.now();
+    return DateFormat('dd.MM.yyyy').format(dateTime);
+  }
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -29,7 +33,7 @@ class AdditionalCommentFormInfo extends StatelessWidget {
         ),
         Spacer(),
         Text(
-          '02.01.24',
+          getDate(),
           style: TextStyle(color: Colors.grey),
         ),
       ],
