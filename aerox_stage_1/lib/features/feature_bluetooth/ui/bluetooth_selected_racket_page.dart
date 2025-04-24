33db35920a030a43673e869c3a7e876e820edb90
RobertoPavonLariosLabtend <roberto.pavon@labtend.com>
2025-04-24 13:00:18 +0200

    sensor with timestamp on bluetooth selectedRacketPage

--- aerox_stage_1/lib/features/feature_bluetooth/ui/bluetooth_selected_racket_page.dart
+++ aerox_stage_1/lib/features/feature_bluetooth/ui/bluetooth_selected_racket_page.dart
@@ -1,6 +1,7 @@
 import 'package:aerox_stage_1/common/ui/error_dialog.dart';
 import 'package:aerox_stage_1/common/utils/bloc/UIState.dart';
 import 'package:aerox_stage_1/domain/models/blob_data_extension.dart';
+import 'package:aerox_stage_1/domain/models/racket_sensor_entity.dart';
 import 'package:aerox_stage_1/domain/use_cases/ble_sensor/start_offline_rtsos_usecase.dart';
 import 'package:aerox_stage_1/features/feature_ble_sensor/feature_blob_database/ui/blob_database_page.dart';
 import 'package:aerox_stage_1/features/feature_ble_sensor/feature_rtsos_hs/ui/rtsos_recording_lobby.dart';
@@ -23,80 +24,89 @@ class BluetoothSelectedRacketPage extends StatelessWidget {
         BlocProvider.of<SelectedEntityPageBloc>(context);
     selectedEntityPageBloc.add(OnGetSelectedRacketSelectedEntityPage());
 
-    return PopScope(
-      canPop: false,
-      child: Scaffold(
-        backgroundColor: Color.fromARGB(255, 225, 245, 255),
-        appBar: AppBar(
-          leading: Builder(
-            builder: (context) => IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {
-                if(selectedEntityPageBloc.state.selectedRacketEntity!=null){
-                  Scaffold.of(context).openDrawer();
+    return BlocListener<SelectedEntityPageBloc, SelectedEntityPageState>(
+      listener: (context, state) {
+        if(state.selectedRacketEntity != null){
+          for( var sensor in state.selectedRacketEntity!.sensors ){
+            selectedEntityPageBloc.add(OnSetTimeStamp(sensor: sensor));
+          }
+        }
+      },
+      child: PopScope(
+          canPop: false,
+          child: Scaffold(
+            backgroundColor: Color.fromARGB(255, 225, 245, 255),
+            appBar: AppBar(
+              leading: Builder(
+                builder: (context) => IconButton(
+                  icon: Icon(Icons.menu),
+                  onPressed: () {
+                    if(selectedEntityPageBloc.state.selectedRacketEntity!=null){
+                      Scaffold.of(context).openDrawer();
+                    }
+                  },
+                ),
+              ),
+              actions:[
+                IconButton(
+                  icon: Icon(Icons.exit_to_app),
+                  onPressed: () {
+                    selectedEntityPageBloc.add(OnDisconnectSelectedRacketSelectedEntityPage());
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ] ,
+            ),
+            drawer: BleTesterActionMenu(),
+            body: BlocListener<SelectedEntityPageBloc, SelectedEntityPageState>(
+              listener: (context, state) {
+                if (state.selectedRacketEntity == null &&
+                    state.uiState.status == UIStatus.error) {
+                  ErrorDialog.showErrorDialog(context, state.uiState.errorMessage);
                 }
               },
+              child: BlocBuilder<SelectedEntityPageBloc, SelectedEntityPageState>(
+                builder: (context, state) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      BlocBuilder<SelectedEntityPageBloc, SelectedEntityPageState>(
+                        builder: (context, state) {
+                          return Center(
+                            child: Column(
+                              children: [
+                                SelectedRacketName(),
+                                BleRecordWithButton( 
+                                  text: 'Grabación con Cámaras', 
+                                  onPressed: (){ Navigator.push(context, MaterialPageRoute(builder: ( context ) => RTSOSRecordingLobby( sampleRate: SampleRate.hz104, ))); }, 
+                                  ),
+                                BleRecordWithButton( 
+                                  text: 'Grabación SIN Cámaras',
+                                  onPressed: (){ Navigator.push(context, MaterialPageRoute(builder: ( context ) => RTSOSRecordingLobby( sampleRate: SampleRate.khz1, ))); }, 
+                                ),
+                                BleRecordWithButton( 
+                                  text: 'Leer Memoria de Sensor',
+      
+                                  onPressed: (){ Navigator.push(context, MaterialPageRoute(builder: ( context ) => BleStoragePage())); }, 
+                                ),
+                                BleRecordWithButton( 
+                                  text: 'Blob Database',
+                                  onPressed: (){ Navigator.push(context, MaterialPageRoute(builder: ( context ) => BlobDatabasePage())); }, 
+                                ),
+                                
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  );
+                },
+              ),
             ),
           ),
-          actions:[
-            IconButton(
-              icon: Icon(Icons.exit_to_app),
-              onPressed: () {
-                selectedEntityPageBloc.add(OnDisconnectSelectedRacketSelectedEntityPage());
-                Navigator.of(context).pop();
-              },
-            ),
-          ] ,
         ),
-        drawer: BleTesterActionMenu(),
-        body: BlocListener<SelectedEntityPageBloc, SelectedEntityPageState>(
-          listener: (context, state) {
-            if (state.selectedRacketEntity == null &&
-                state.uiState.status == UIStatus.error) {
-              ErrorDialog.showErrorDialog(context, state.uiState.errorMessage);
-            }
-          },
-          child: BlocBuilder<SelectedEntityPageBloc, SelectedEntityPageState>(
-            builder: (context, state) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  BlocBuilder<SelectedEntityPageBloc, SelectedEntityPageState>(
-                    builder: (context, state) {
-                      return Center(
-                        child: Column(
-                          children: [
-                            SelectedRacketName(),
-                            BleRecordWithButton( 
-                              text: 'Grabación con Cámaras', 
-                              onPressed: (){ Navigator.push(context, MaterialPageRoute(builder: ( context ) => RTSOSRecordingLobby( sampleRate: SampleRate.hz104, ))); }, 
-                              ),
-                            BleRecordWithButton( 
-                              text: 'Grabación SIN Cámaras',
-                              onPressed: (){ Navigator.push(context, MaterialPageRoute(builder: ( context ) => RTSOSRecordingLobby( sampleRate: SampleRate.khz1, ))); }, 
-                            ),
-                            BleRecordWithButton( 
-                              text: 'Leer Memoria de Sensor',
-  
-                              onPressed: (){ Navigator.push(context, MaterialPageRoute(builder: ( context ) => BleStoragePage())); }, 
-                            ),
-                            BleRecordWithButton( 
-                              text: 'Blob Database',
-                              onPressed: (){ Navigator.push(context, MaterialPageRoute(builder: ( context ) => BlobDatabasePage())); }, 
-                            ),
-                            
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              );
-            },
-          ),
-        ),
-      ),
     );
   }
 }
